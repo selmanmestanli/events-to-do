f5414373df98d07dbaecfc87df995a1a4e016563
selmanmestanli <selmanmestanli@gmail.com>
2021-12-03 23:07:47 +0100

    Create EventCard component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+    <span>{{ event.attendees.length }} attending</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Beach Cleanup',
+        date: 'Tues Aug 19, 2021',
+        time: '6:00',
+        attendees: [
+          { id: 'abc123', name: 'Adam Jahr' },
+          { id: 'def456', name: 'Gregg Pollack' },
+        ],
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+h4 {
+  color: green;
+}
+</style>
