78523350649bf0b9e4b942e01532a76d0d2057a5
selmanmestanli <selmanmestanli@gmail.com>
2021-12-06 18:05:53 +0100

    Implement event object as props

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
@@ -13,6 +13,8 @@
 
 <script>
 export default {
+  props: {
+    event: Object,
   },
 }
 </script>
