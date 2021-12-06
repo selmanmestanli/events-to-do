34868ef7944f53f56a15bced16d9cd47786e10a3
selmanmestanli <selmanmestanli@gmail.com>
2021-12-06 17:50:44 +0100

    Fix typo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav" class="nav">
-    <router-link to="/" class="brand">Events To Do</router-link> |
+    <router-link to="/" class="brand">Events To Do</router-link>
     <nav>
       <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-create' }">Create</router-link>
