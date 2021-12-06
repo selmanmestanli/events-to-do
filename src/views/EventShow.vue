72cc104107f43acf928ec4401dcdcfef2688cbb7
selmanmestanli <selmanmestanli@gmail.com>
2021-12-06 17:56:58 +0100

    Implement EventServices to the EventShow view

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,7 +3,22 @@
 </template>
 
 <script>
+import EventService from '@/services/EventService.js'
 export default {
   props: ['id'],
+  data() {
+    return {
+      event: {},
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data
+      })
+      .catch((error) => {
+        console.log('There is an error:', error.response)
+      })
+  },
 }
 </script>
