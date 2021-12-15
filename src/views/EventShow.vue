50710b83abf72a44eb8d00ed37813172999f3638
selmanmestanli <selmanmestanli@gmail.com>
2021-12-15 15:38:34 +0100

    Prevent dublication by setting store commit into the component

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -33,23 +33,14 @@
 </template>
 
 <script>
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 export default {
   props: ['id'],
-  data() {
-    return {
-      event: {},
-    }
-  },
+
   created() {
-    EventService.getEvent(this.id)
-      .then((response) => {
-        this.event = response.data
-      })
-      .catch((error) => {
-        console.log('There is an error:', error.response)
-      })
+    this.$store.dispatch('fetchEvent', this.id)
   },
+  computed: mapState(['event']),
 }
 </script>
 
