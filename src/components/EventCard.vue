12b7c1425086a4b7928a43d00a07e38cdfdaaaf7
selmanmestanli <selmanmestanli@gmail.com>
2021-12-15 15:33:32 +0100

    Implement style class

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -5,7 +5,7 @@
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-      <h4>{{ event.title }}</h4>
+      <h4 class="title">{{ event.title }}</h4>
       <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
