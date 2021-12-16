6919f9336b07c2bec1a6e9cb32fcf201d52f1c1c
selmanmestanli <selmanmestanli@gmail.com>
2021-12-16 17:45:33 +0100

    Implement modules to the vuex store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import EventService from '@/services/EventService.js'
+import * as user from '@/store/modules/user.js'
+import * as event from '@/store/modules/event.js'
+import * as notification from '@/store/modules/notification.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    user,
+    event,
+    notification,
+  },
   state: {
     categories: [
       'sustainability',
